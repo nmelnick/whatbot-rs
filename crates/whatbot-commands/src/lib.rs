e7784848825ef880bb229e2d7f0048c689594767
Nick Melnick <nmelnick@users.noreply.github.com>
2026-06-03 23:03:34 -0400

    Add Excuse command (#2)
    * Find excuses
    
    * A basic test

--- crates/whatbot-commands/src/lib.rs
+++ crates/whatbot-commands/src/lib.rs
@@ -1,10 +1,12 @@
 pub mod echo;
+pub mod excuse;
 pub mod factoid;
 pub mod help;
 pub mod karma;
 pub mod seen;
 
 pub use echo::Echo;
+pub use excuse::Excuse;
 pub use factoid::{Factoid, FactoidListener, FactoidScratch};
 pub use help::Help;
 pub use karma::Karma;
